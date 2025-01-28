ce4dccf94dc2582ef0d22fb46314c5957b944e6e
Andres M Bran <doncamilom@gmail.com>
2025-01-28 20:37:05 +0100

    make some specific dataset classes

---
+++ src/open_r1/dataset/forward.py
@@ -0,0 +1,33 @@
+
+from .base import CustomDatasetLoader
+from typing import Dict
+import re
+
+
+class ForwardReactionDataset(CustomDatasetLoader):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.question_template = "What is the product of the following reaction? <|begin_smiles|>{}<|end_smiles|>"
+
+    def process_line(self, line: str) -> str:
+        """Process a line from the source file."""
+        rm_space = re.sub(" +", "", line)
+        return rm_space
+
+    def read_files(self, src_file: str, tgt_file: str) -> Dict:
+        """Read source and target files and create dataset dictionary."""
+        with open(src_file, 'r', encoding='utf-8') as f:
+            problems = [self.question_template.format(self.process_line(line)) for line in f.readlines()]
+            
+        with open(tgt_file, 'r', encoding='utf-8') as f:
+            solutions = [self.process_line(line) for line in f.readlines()]
+            
+        # Create empty messages list for each example
+        messages = [[] for _ in range(len(problems))]
+        
+        return {
+            'problem': problems,
+            'solution': solutions,
+            'messages': messages
+        }
