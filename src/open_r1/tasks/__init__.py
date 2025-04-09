477b2f23cd59ec1330776313f5d081c160f5a3e3
Jeremy Goumaz <56827471+jgoumaz@users.noreply.github.com>
2025-04-09 17:33:05 +0200

    Solving the merge conflicts
    - launch.slurm recreated (copy of launch.template_cscs.slurm). When this file is missing, the merge is blocked by a conflict
    - src/open_r1/tasks/__init__.py modified (tasks reordered to remove the merge conflicts)

--- src/open_r1/tasks/__init__.py
+++ src/open_r1/tasks/__init__.py
@@ -4,16 +4,17 @@ from .reactions.canon_mcqa import CanonicalizeSmilesMCQA
 from .reactions.iupac2smi import Iupac2Smiles, Iupac2SmilesWithTags
 from .reactions.smi_permute import PermuteSmiles
 from .smiles_understanding.smiles_hydrogen import SmilesHydrogen
-
+from .kinetic_data.kinetic_data_classification import KineticDataClassification
 
 # Task keys as specified in the task recipes and documentation
 CHEMTASKS = {
+    "ForwardReactionWithTags": ForwardReactionWithTags,
+    "Iupac2SmilesWithTags": Iupac2SmilesWithTags,
+    "PermuteSmiles": PermuteSmiles,
     "rxnpred": ForwardReaction,
     "canonic": CanonicalizeSmiles,
     "canonmc": CanonicalizeSmilesMCQA,
     "iupacsm": Iupac2Smiles,
     "smhydrogen": SmilesHydrogen,
-    "ForwardReactionWithTags": ForwardReactionWithTags,
-    "Iupac2SmilesWithTags": Iupac2SmilesWithTags,
-    "PermuteSmiles": PermuteSmiles
+    "kinetic": KineticDataClassification,
 }
