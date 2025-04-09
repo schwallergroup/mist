a9e9452866d960dfa1f74e50633d0dc582cbdce4
Jeremy Goumaz <56827471+jgoumaz@users.noreply.github.com>
2025-04-09 17:14:21 +0200

    Create __init__.py

---
+++ src/open_r1/tasks/__init__.py
@@ -0,0 +1,19 @@
+from .reactions.forward import ForwardReaction, ForwardReactionWithTags
+from .reactions.canonical import CanonicalizeSmiles
+from .reactions.canon_mcqa import CanonicalizeSmilesMCQA
+from .reactions.iupac2smi import Iupac2Smiles, Iupac2SmilesWithTags
+from .reactions.smi_permute import PermuteSmiles
+from .smiles_understanding.smiles_hydrogen import SmilesHydrogen
+
+
+# Task keys as specified in the task recipes and documentation
+CHEMTASKS = {
+    "rxnpred": ForwardReaction,
+    "canonic": CanonicalizeSmiles,
+    "canonmc": CanonicalizeSmilesMCQA,
+    "iupacsm": Iupac2Smiles,
+    "smhydrogen": SmilesHydrogen,
+    "ForwardReactionWithTags": ForwardReactionWithTags,
+    "Iupac2SmilesWithTags": Iupac2SmilesWithTags,
+    "PermuteSmiles": PermuteSmiles
+}
