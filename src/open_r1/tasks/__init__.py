284d782c92d1721d4694c04fb05b968da17971bf
Jeremy Goumaz <56827471+jgoumaz@users.noreply.github.com>
2025-04-09 17:22:06 +0200

    Update __init__.py

--- src/open_r1/tasks/__init__.py
+++ src/open_r1/tasks/__init__.py
@@ -1,7 +1,8 @@
-from .reactions.forward import ForwardReaction
+from .reactions.forward import ForwardReaction, ForwardReactionWithTags
 from .reactions.canonical import CanonicalizeSmiles
 from .reactions.canon_mcqa import CanonicalizeSmilesMCQA
-from .reactions.iupac2smi import Iupac2Smiles
+from .reactions.iupac2smi import Iupac2Smiles, Iupac2SmilesWithTags
+from .reactions.smi_permute import PermuteSmiles
 from .smiles_understanding.smiles_hydrogen import SmilesHydrogen
 from .kinetic_data.kinetic_data_classification import KineticDataClassification
 
@@ -12,5 +13,8 @@ CHEMTASKS = {
     "canonmc": CanonicalizeSmilesMCQA,
     "iupacsm": Iupac2Smiles,
     "smhydrogen": SmilesHydrogen,
+    "ForwardReactionWithTags": ForwardReactionWithTags,
+    "Iupac2SmilesWithTags": Iupac2SmilesWithTags,
+    "PermuteSmiles": PermuteSmiles,
     "kinetic": KineticDataClassification,
 }
