97b040799761cdc4362350dcad5b9e8767d99ee8
Jeremy Goumaz <56827471+jgoumaz@users.noreply.github.com>
2025-04-11 16:56:04 +0200

    Removed scripts not needed in the main (not used anywhere in the repo)

--- simple_launch.sh
+++
@@ -1,9 +0,0 @@
-HF_HUB_ENABLE_HF_TRANSFER=1 ACCELERATE_LOG_LEVEL=info TRANSFORMERS_VERBOSITY=info accelerate launch \
-    --config_file configs/deepspeed_zero3.yaml \
-    --main_process_port 29501 \
-    --num_processes 1 \
-    src/open_r1/run_r1_grpo.py \
-    --config recipes/smi_permute.test.yaml \
-    --model_name_or_path Qwen/Qwen2.5-0.5B \
-    --output_dir output/grpo \
-    --dataset_id_or_path /home/vu/Documents/open-r1/data/CRLLM-PubChem-compounds1M-simple.csv
--- submit_job.sh
+++
@@ -1,4 +0,0 @@
-sbatch \
-    --nodes=1 \
-    --time=08:00:00 \
-    launch.slurm Qwen2.5-0.5B rxnpred_with_tags.small 0
