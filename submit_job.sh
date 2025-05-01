d743643a9558109eccd1d6021a383bc4e38e0d09
Nguyen Xuan Vu Nguyen <nxnguyen@kuma2.kuma.cluster>
2025-05-01 17:39:46 +0200

    minor

--- submit_job.sh
+++ submit_job.sh
@@ -1,4 +1,4 @@
 sbatch \
     --nodes=2 \
-    --time=06:00:00 \
-    launch_kuma.slurm Qwen2.5-3B_pretrained-v5 rxnpred_with_tags
+    --time=01:00:00 \
+    launch_kuma.slurm Qwen2.5-3B_pretrained-v5 rxnpred 0 fg_tagged
