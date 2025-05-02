5517f8ec2d546c34f032ee23a53cb284f72d3ac6
XuanVuNguyen <xuanvunguyen.1834@gmail.com>
2025-05-02 17:14:37 +0200

    revisit iupacsm

--- src/open_r1/tasks/reactions/utils.py
+++ src/open_r1/tasks/reactions/utils.py
@@ -10,3 +10,12 @@ def tanimoto_sim(mol1, mol2):
     fp2 = AllChem.GetMorganFingerprintAsBitVect(mol2, radius=2, useChirality=True)
 
     return DataStructs.TanimotoSimilarity(fp1, fp2)
+
+def tanimoto_score(mol1: str, mol2: str, beta=1):
+    if (
+        Chem.MolFromSmiles(mol1) is None
+        or Chem.MolFromSmiles(mol2) is None
+    ):
+        return 0.0
+    sim = tanimoto_sim(mol1, mol2)
+    return sim**beta
