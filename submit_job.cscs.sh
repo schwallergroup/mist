bd9b0aceb51b62e380630ffa08eaf3584544da3b
Nguyen Xuan Vu Nguyen <nnguyenx@clariden-ln003.cscs.ch>
2025-05-02 20:37:40 +0200

    frequent logs for rxnpred

--- submit_job.cscs.sh
+++ submit_job.cscs.sh
@@ -1,4 +1,5 @@
 sbatch \
     --nodes=4 \
     --time=08:00:00 \
-    launch_CSCS.slurm Qwen2.5-3B_pretrained-v5 rxnpred_with_tags.4nodes_cscs
+    --partition=normal \
+    launch_CSCS.slurm Qwen2.5-3B_pretrained-v6-1 rxnpred 0 tagged
