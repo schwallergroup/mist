eecf7991cccff8589a057b8dd05d630e77ae6dd6
Nguyen Xuan Vu Nguyen <nnguyenx@clariden-ln002.cscs.ch>
2025-05-06 21:42:02 +0200

    vllm0.15.0 is faster!

--- submit_job.cscs.sh
+++ submit_job.cscs.sh
@@ -1,5 +1,5 @@
 sbatch \
-    --nodes=4 \
-    --time=08:00:00 \
-    --partition=normal \
-    launch_CSCS.slurm Qwen2.5-3B_pretrained-v6-1 rxnpred 0 tagged
+    --nodes=2 \
+    --time=00:40:00 \
+    --partition=debug \
+    launch_CSCS.slurm Qwen2.5-3B_pretrained-v6-1 iupacsm.long.vllm15 0 tagged
