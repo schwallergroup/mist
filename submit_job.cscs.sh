742a11b31c27c2fae6545849c35b2da6f136ce4a
Nguyen Xuan Vu Nguyen <nnguyenx@clariden-ln002.cscs.ch>
2025-05-08 02:13:06 +0200

    1 batch_size 8 generations

--- submit_job.cscs.sh
+++ submit_job.cscs.sh
@@ -1,5 +1,5 @@
 sbatch \
-    --nodes=2 \
-    --time=00:40:00 \
-    --partition=debug \
-    launch_CSCS.slurm Qwen2.5-3B_pretrained-v6-1 iupacsm.long.vllm15 0 tagged
+    --nodes=4 \
+    --time=10:00:00 \
+    --partition=normal \
+    launch_CSCS.slurm Qwen2.5-3B_pretrained-v6-1 iupacsm.long 0 tagged
