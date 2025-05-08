02f593bd2f2783ced17f85345155fcb6088b2654
Nguyen Xuan Vu Nguyen <nnguyenx@clariden-ln002.cscs.ch>
2025-05-08 16:28:03 +0200

    continue run from kuma on cscs

--- submit_job.cscs.sh
+++ submit_job.cscs.sh
@@ -2,4 +2,4 @@ sbatch \
     --nodes=4 \
     --time=10:00:00 \
     --partition=normal \
-    launch_CSCS.slurm Qwen2.5-3B_pretrained-v6-1 iupacsm.long 0 tagged
+    launch_CSCS.slurm Qwen2.5-3B_pretrained-v6-1 rxnpred.long kuma_822876 tagged
