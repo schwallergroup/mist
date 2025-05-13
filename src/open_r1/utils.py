e781a69949821e6e9a5b2e2ce6a1647fd120a454
Jeremy Goumaz <56827471+jgoumaz@users.noreply.github.com>
2025-05-14 01:27:52 +0200

    💥compute_loss updated in ExtendedGRPOTrainer

--- src/open_r1/utils.py
+++ src/open_r1/utils.py
@@ -18,8 +18,8 @@ from importlib.metadata import version
 import torch
 from trl.data_utils import apply_chat_template, is_conversational, maybe_apply_chat_template
 from accelerate.utils import broadcast_object_list, gather_object
-from .trl_functions14 import unwrap_model_for_generation, pad, selective_log_softmax
-from .trl17_profiling import profiling_decorator
+# from trl_functions14 import unwrap_model_for_generation, pad, selective_log_softmax TODO: add relative imports compatibility
+# from trl17_profiling import profiling_decorator TODO: add relative imports compatibility
 
 MetricFunc = Callable[[], dict]
 
@@ -43,6 +43,7 @@ class ExtendedGRPOTrainer(GRPOTrainer):
             print("[ExtendedGRPOTrainer.__init__] Deepspeed ZeRO3 is enabled")
         else:
             print("[ExtendedGRPOTrainer.__init__] Deepspeed ZeRO3 is NOT enabled")
+        self.logging_print_once = []
 
         # Set up logging of good completions
         self.logging_completions = {
@@ -228,6 +229,8 @@ class ExtendedGRPOTrainer(GRPOTrainer):
         """
         Copy of _get_per_token_logps from TRL 0.17.0
         """
+        # TODO: add compatibility with this function in _compute_loss_modified
+        raise Exception(f"[ExtendedGRPOTrainer._get_per_token_logps] This method is not used in the current version of the code -> no support yet")
         batch_size = batch_size or input_ids.size(0)  # Chunk inputs into smaller batches to reduce memory peak
         all_logps = []
         for i in range(0, input_ids.size(0), batch_size):
@@ -410,10 +413,19 @@ class ExtendedGRPOTrainer(GRPOTrainer):
         else:
             raise ValueError(f"Unknown loss type: {self.loss_type}")
 
+        # Logging print (once)
+        if 'print_completion_mask_shape' not in self.logging_print_once:
+            print(f"[ExtendedGRPOTrainer._compute_loss_modified] completion_mask shape: {completion_mask.shape.tolist()}")
+            self.logging_print_once.append('print_completion_mask_shape')
+        if 'print_prompt_inputs_attention_mask_shape' not in self.logging_print_once:
+            print(f"[ExtendedGRPOTrainer._compute_loss_modified] prompt_inputs[\"attention_mask\"] shape: {prompt_inputs['attention_mask'].shape.tolist()}")
+            self.logging_print_once.append('print_prompt_inputs_attention_mask_shape')
+
         # Log the metrics
         completion_length = self.accelerator.gather_for_metrics(completion_mask.sum(1)).float().mean().item()
         self._metrics["completion_length"].append(completion_length)
 
+        # TODO: add this logging in wandb (require attention_mask)
         # self.state.num_input_tokens_seen += self.accelerator.gather_for_metrics(attention_mask.sum()).sum().item()
         # self._metrics["num_tokens"] = [self.state.num_input_tokens_seen]
 
@@ -567,3 +579,162 @@ def load_sampling_params_config(training_args: ExtendedGRPOConfig):
         assert not os.path.isfile(f"{sampling_params_dir}/default.json"), f"The file sampling_params/default.json exists. The global default sampling_params can't be overwritten, please remove this file (or put the config in a new file)."
 
     return training_args
+
+
+"""
+FULL COPY of trl14_functions.py
+(copied there because easier than modifying PYTHONPATH or relative imports for the moments)
+TODO: add relative imports compatibility
+"""
+
+import itertools
+from contextlib import contextmanager
+from typing import TYPE_CHECKING, Literal, Optional, Union
+import deepspeed
+from accelerate import Accelerator
+from deepspeed.runtime.engine import DeepSpeedEngine
+from torch.nn.parallel.distributed import DistributedDataParallel
+from trl.models import PreTrainedModelWrapper
+
+
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+
+# Copy of trl/models/utils.py
+def remove_hooks(model: "DeepSpeedEngine") -> None:
+    """Removes the optimizer hooks from a DeepSpeed ZeRO-3 model."""
+    if model.optimizer is not None and hasattr(model.optimizer, "parameter_offload"):
+        optimizer_offload = model.optimizer.parameter_offload
+    elif model.optimizer is not None:
+        optimizer_offload = model.optimizer
+
+    for param in iter_params(optimizer_offload.module, recurse=True):
+        param.ds_active_sub_modules.clear()
+
+    for hook in optimizer_offload.forward_hooks:
+        hook.remove()
+    for hook in optimizer_offload.backward_hooks:
+        hook.remove()
+
+    optimizer_offload.forward_hooks = []
+    optimizer_offload.backward_hooks = []
+def get_all_parameters(sub_module, recurse=False):
+    return itertools.chain(sub_module.named_parameters(recurse=recurse), sub_module.ds_external_parameters())
+def iter_params(module, recurse=False):
+    return [param for _, param in get_all_parameters(module, recurse)]
+def add_hooks(model: "DeepSpeedEngine") -> None:
+    """Adds the optimizer hooks from a DeepSpeed ZeRO-3 model."""
+    if model.optimizer is not None and hasattr(model.optimizer, "parameter_offload"):
+        optimizer_offload = model.optimizer.parameter_offload
+    elif model.optimizer is not None:
+        optimizer_offload = model.optimizer
+    optimizer_offload._register_hooks_recursively(optimizer_offload.module)
+@contextmanager
+def unwrap_model_for_generation(
+    model: Union["DistributedDataParallel", "DeepSpeedEngine"],
+    accelerator: "Accelerator",
+    is_peft_model: bool = False,
+    gather_deepspeed3_params: bool = True,
+) -> Union["PreTrainedModelWrapper", "DeepSpeedEngine"]:
+    """Context manager to unwrap a model for generation.
+    For ZeRO-3 models, we gather the weights once to speed up generation.
+    """
+    unwrapped_model = accelerator.unwrap_model(model)
+    if is_peft_model:
+        unwrapped_model.pretrained_model.disable_adapter()
+    if accelerator.state.deepspeed_plugin is not None and accelerator.state.deepspeed_plugin.zero_stage == 3:
+        if not gather_deepspeed3_params:
+            yield accelerator.unwrap_model(model)
+        else:
+            with deepspeed.zero.GatheredParameters(model.parameters()):
+                remove_hooks(model)
+                yield accelerator.unwrap_model(model)
+                add_hooks(model)
+    else:
+        yield unwrapped_model
+
+
+# Copy of trl/trainer/utils.py
+def pad(tensors: list[torch.Tensor], padding_value: int = 0, padding_side: str = "right") -> torch.Tensor:
+    """
+    Pads a list of tensors to the same shape along the first dimension.
+
+    Args:
+        tensors (`list[torch.Tensor]`):
+            List of input tensors to pad.
+        padding_value (`int`):
+            Value to use for padding. Default is 0.
+        padding_side (`str`):
+            Side on which to add padding. Must be 'left' or 'right'. Default is 'right'.
+
+    Returns:
+        `torch.Tensor`:
+            A single tensor containing the padded tensors.
+
+    Examples:
+        >>> import torch
+        >>> pad([torch.tensor([1, 2, 3]), torch.tensor([4, 5])])
+        tensor([[1, 2, 3],
+                [4, 5, 0]])
+        >>> pad([torch.tensor([[1, 2], [3, 4]]), torch.tensor([[5, 6]])])
+        tensor([[[1, 2],
+                [3, 4]],
+
+                [[5, 6],
+                [0, 0]]])
+    """
+    # Determine the maximum shape for each dimension
+    output_shape = np.max([t.shape for t in tensors], 0).tolist()
+
+    # Create an output tensor filled with the padding value
+    output = torch.full((len(tensors), *output_shape), padding_value, dtype=tensors[0].dtype, device=tensors[0].device)
+
+    for i, t in enumerate(tensors):
+        # Determine the slice for the sequence dimension
+        if padding_side == "left":
+            seq_slice = slice(output_shape[0] - t.shape[0], output_shape[0])
+        elif padding_side == "right":
+            seq_slice = slice(0, t.shape[0])
+        else:
+            raise ValueError("padding_side must be 'left' or 'right'")
+
+        slices = (seq_slice,) + tuple(slice(0, s) for s in t.shape[1:])
+        output[i][slices] = t
+
+    return output
+def selective_log_softmax(logits, index):
+    """
+    A memory-efficient implementation of the common `log_softmax -> gather` operation.
+
+    This function is equivalent to the following naive implementation:
+    ```python
+    logps = torch.gather(logits.log_softmax(-1), dim=-1, index=index.unsqueeze(-1)).squeeze(-1)
+    ```
+
+    Args:
+        logits (`torch.Tensor`):
+            Logits tensor of shape `(..., num_classes)`.
+        index (`torch.Tensor`):
+            Index tensor of shape `(...)`, specifying the positions to gather from the log-softmax output.
+
+    Returns:
+        `torch.Tensor`:
+            Gathered log probabilities with the same shape as `index`.
+    """
+    if logits.dtype in [torch.float32, torch.float64]:
+        selected_logits = torch.gather(logits, dim=-1, index=index.unsqueeze(-1)).squeeze(-1)
+        # loop to reduce peak mem consumption
+        logsumexp_values = torch.stack([torch.logsumexp(lg, dim=-1) for lg in logits])
+        per_token_logps = selected_logits - logsumexp_values  # log_softmax(x_i) = x_i - logsumexp(x)
+    else:
+        # logsumexp approach is unstable with bfloat16, fall back to slightly less efficent approach
+        per_token_logps = []
+        for row_logits, row_labels in zip(logits, index):  # loop to reduce peak mem consumption
+            row_logps = F.log_softmax(row_logits, dim=-1)
+            row_per_token_logps = row_logps.gather(dim=-1, index=row_labels.unsqueeze(-1)).squeeze(-1)
+            per_token_logps.append(row_per_token_logps)
+        per_token_logps = torch.stack(per_token_logps)
+    return per_token_logps
+
