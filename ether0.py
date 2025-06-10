a22228586eee49f3b5de1bafa49776e8e4791605
Nguyen Xuan Vu Nguyen <nnguyenx@clariden-ln002.cscs.ch>
2025-06-10 11:51:08 +0200

    quick test

---
+++ ether0.py
@@ -0,0 +1,52 @@
+import json
+import os
+import shutil
+import random
+from vllm import LLM, SamplingParams, EngineArgs
+from rdkit import Chem
+import re
+import pickle
+from open_r1.tasks import ForwardReaction
+from argparse import ArgumentParser
+
+def main():
+    
+    llm = LLM(
+        model="futurehouse/ether0",
+        generation_config="auto",
+        download_dir="/iopsstor/scratch/cscs/nnguyenx/models/"
+    )
+    # llm = LLM(model="futurehouse/ether0", download_dir="/iopsstor/scratch/cscs/nnguyenx/models/")
+    sampling_params = llm.get_default_sampling_params()
+    sampling_params.max_tokens=2048
+    
+    with open("ether0/prompt.txt", "r") as f:
+        prompt = f.read()
+        
+    conversation = [
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+    
+    outputs = llm.chat(conversation, sampling_params=sampling_params)
+    
+    output = outputs[0].outputs[0].text
+    
+    model_prompt = outputs[0].prompt
+    
+    response = {
+        "model": "futurehouse/ether0",
+        "prompt": model_prompt,
+        "sampling_params": str(sampling_params),
+        "output": output
+    }
+    response_print = json.dumps(response, indent=2)
+    
+    with open("ether0/output.json", "w") as f:
+        f.write(response_print)
+
+if __name__ == "__main__":
+    main()
+    
