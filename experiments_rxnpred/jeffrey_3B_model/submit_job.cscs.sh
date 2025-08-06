fbdd9325e44ada4243505e4ee242809483703b57
XuanVuNguyen <xuanvunguyen.1834@gmail.com>
2025-08-06 17:06:40 +0200

    GRPO on jeffrey pretrained models

--- experiments_rxnpred/jeffrey_3B_model/submit_job.cscs.sh
+++ experiments_rxnpred/jeffrey_3B_model/submit_job.cscs.sh
@@ -3,4 +3,4 @@ sbatch \
     --nodes=4 \
     --time=10:00:00 \
     --partition=normal \
-    launch_CSCS.slurm Qwen2.5-3B_pretrained-v6-1 iupacsm.beta04 0 tagged_no_okay
+    launch_CSCS.slurm Qwen2.5-3B_pretrained-vj1-sfte8 rxnpred 0 tagged
